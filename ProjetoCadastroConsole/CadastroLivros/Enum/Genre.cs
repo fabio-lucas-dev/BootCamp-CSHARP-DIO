2166d844be87ba3d0c99c816785144ef481a48cf
Fábio <fabiolucs19@gmail.com>
2022-01-26 20:24:45 -0300

    Atualizando com correções

--- ProjetoCadastroConsole/CadastroLivros/Enum/Genre.cs
+++ ProjetoCadastroConsole/CadastroLivros/Enum/Genre.cs
@@ -2,16 +2,16 @@ namespace CadastroLivros.Enum
 {
     public enum Genre
     {
-        Ação = 1,
+        Acao = 1,
         Aventura = 2,
-        Comédia = 3,
-        Documentário = 4,
+        Comedia = 3,
+        Documentario = 4,
         Drama = 5,
         Faroeste = 6,
         Fantasia = 7,
-        Ficção_Científica = 8,
-        História_em_Quadrinhos = 9,
-        Mangá = 10,
+        Ficcao_Cientifica = 8,
+        Historia_em_Quadrinhos = 9,
+        Manga = 10,
         Romance = 11,
         Suspense = 12,
         Terror = 13
